0e1de6475b8ea3228cb517ec47a78ef3bd2b52ca
Philip Jacob <philipjacob@Philips-MacBook-Pro.local>
2016-11-17 18:18:01 -0500

    setup

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -7,21 +7,14 @@ export default class Navbar extends Component {
 
   render() {
     return (
-      <nav>
+      <nav className="transparent">
         <div className="nav-wrapper">
-          <a href="/" className="brand-logo">Logo</a>
-          <a href="/" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-          <ul className="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">Javascript</a></li>
-            <li><a href="mobile.html">Mobile</a></li>
-          </ul>
-          <ul className="side-nav" id="mobile-demo">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">Javascript</a></li>
-            <li><a href="mobile.html">Mobile</a></li>
+          <a href="#" className="brand-logo">Logo</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="sass.html">What Is It</a></li>
+            <li><a href="badges.html">Why Use It</a></li>
+            <li><a href="collapsible.html">About Us</a></li>
+            <li><a href="collapsible.html">Login / Signup</a></li>
           </ul>
         </div>
       </nav>
