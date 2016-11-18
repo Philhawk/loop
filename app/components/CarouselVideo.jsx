aa32336d89c76060d314cdfc11d25e794935070c
Philip Jacob <philipjacob@Philips-MacBook-Pro.local>
2016-11-18 17:02:31 -0500

    image slideshow

--- app/components/CarouselVideo.jsx
+++ app/components/CarouselVideo.jsx
@@ -16,19 +16,28 @@ class CarouselVideo extends Component {
           displayQuantityOfSide={1}
           enableHeading={true}
           media={{
+            '@media (max-width: 1200px)': {
+              width: '50vw',
+              height: '300px'
+            },
+            '@media (max-width: 1800px)': {
+              width: '50vw',
+              height: '300px'
+            },
             '@media (max-width: 850px)': {
               width: '60vw',
               height: '300px'
             },
             '@media (max-width: 700px)': {
               width: '80vw',
-              height: '300px'
+              height: '200px'
             }
           }}
+
           >
-          <img src='img/vr.jpg' className='text' alt='Album one' data-action="https://facebook.github.io/react/"/>
-          <img src='img/vr.jpg' className='text' alt='Album two' data-action="http://passer.cc"/>
-          <img src='img/vr.jpg' className='text' alt='Album three' data-action="https://doce.cc/"/>
+          <img src='img/vr.jpg' alt='Album one' data-action="https://facebook.github.io/react/"/>
+          <img src='img/vr.jpg' alt='Album two' data-action="http://passer.cc"/>
+          <img src='img/vr.jpg' alt='Album three' data-action="https://doce.cc/"/>
         </Coverflow>
       </StyleRoot>
     )
