9e1876b72b60791638897e7b37462d18d526f970
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-11-18 10:35:22 -0500

    merging

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -8,7 +8,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="homepage">
+      <div className="homePage">
         <Navbar/>
       </div>
     );
--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import LoginModal from '../LoginModal/LoginModal'
 
 
 export default class Navbar extends Component {
   constructor() {
     super();
   }
+
 
   render() {
     return (
@@ -16,21 +18,12 @@ export default class Navbar extends Component {
               Loops
             </a>
             <ul className="right hide-on-med-and-down">
-              <li><a href="">Why use it?</a></li>
-              <li><a href="">How it works?</a></li>
+              <li><a href="">Why use it</a></li>
+              <li><a href="">How it works</a></li>
               <li><a href="">About Us</a></li>
               <li><a className="waves-effect waves-light btn" href="#modal1">Login</a></li>
-
             </ul>
-            <div id="modal1" className="modal">
-              <div className="modal-content">
-                <h4>Modal Header</h4>
-                <p>A bunch of text</p>
-              </div>
-              <div className="modal-footer">
-                <a href="#!" className=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-              </div>
-            </div>
+            <LoginModal/>
           </div>
         </nav>
       </div>
