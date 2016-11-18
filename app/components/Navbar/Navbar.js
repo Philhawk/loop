b2833bf1caf4c0215328c516af2932289bf5392e
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-11-18 00:01:13 -0500

    more changes

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ export default class Navbar extends Component {
       <div className="navbar-fixed">
         <nav className="transparent z-depth-0">
           <div className="nav-wrapper">
-            <a href="#!" className="brand-logo">Logo</a>
+            <a href="/" className="brand-logo"><img src="../../public/images/logo.png"></img></a>
             <ul className="right hide-on-med-and-down">
               <li><a href="sass.html">Why use it?</a></li>
               <li><a href="sass.html">How it works?</a></li>
