41db866a9e5c5a85f95c3eb7491bcd9fdadc3851
Philhawk <pkjacob@gmail.com>
2016-11-28 13:31:55 -0500

    time formatted

--- app/components/Loop/LoopAnalysis.jsx
+++ app/components/Loop/LoopAnalysis.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 class LoopAnalysis extends Component {
   constructor() {
@@ -8,11 +9,16 @@ class LoopAnalysis extends Component {
 
 
   render() {
-    console.log("PROPS", this.props)
+    let timeNow = moment();
+    let lectureStart = moment(this.props.session.timeStarted)
+    let duration = moment.duration(timeNow - lectureStart)
+    let formattedTimeDuration = moment(duration.asMilliseconds()).format('mm:ss')
+
     return (
 
       <div>
-          <h1>Time: { Date.now - this.props.session.timeStarted }</h1>
+          <h1>Time: { formattedTimeDuration }</h1>
+
           {
           this.props.studentQuestions.map((question) => (
             <p> {question.content}</p>
