4c51b29877b9c2f431cecfea966ebba9beeeff2e
Philhawk <pkjacob@gmail.com>
2016-11-28 14:33:57 -0500

    post-loop-analysis done

--- app/components/Loop/LoopAnalysis.jsx
+++ app/components/Loop/LoopAnalysis.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import Slider from 'material-ui/Slider';
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import Subheader from 'material-ui/Subheader';
+import Avatar from 'material-ui/Avatar';
+import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
 
 class LoopAnalysis extends Component {
   constructor() {
@@ -14,17 +25,57 @@ class LoopAnalysis extends Component {
     let duration = moment.duration(timeNow - lectureStart)
     let formattedTimeDuration = moment(duration.asMilliseconds()).format('mm:ss')
 
+    const styles = {
+      headline: {
+        fontSize: 24,
+        paddingTop: 16,
+        marginBottom: 12,
+        fontWeight: 400,
+      },
+    };
+
+
     return (
 
-      <div>
-          <h1>Time: { formattedTimeDuration }</h1>
+        <div>
+          <form className="post-loop-analysis">
+            <h3 className="post-loop-analysis-heading">Well done!</h3>
+            <Tabs>
+              <Tab label="Statistics" >
+                <div>
+                  <p>
+                    Duration: {formattedTimeDuration}
+                  </p>
+                </div>
+              </Tab>
+              <Tab label="Questions" >
+                <div>
+                  {
+                    this.props.studentQuestions.map((question) => ((
 
-          {
-          this.props.studentQuestions.map((question) => (
-            <p> {question.content}</p>
-          ))
-        }
-      </div>
+                      <List>
+                        <ListItem
+                          leftAvatar={<Avatar src="img/user.jpg" />}
+                          secondaryText={
+                            <p>
+                              <br/>
+                              <span style={{color: darkBlack}}>{question.content}</span>
+
+                            </p>
+                          }
+                          secondaryTextLines={2}
+                        />
+                        <Divider inset={true} />
+                      </List>
+                    ))
+                  )
+                }
+                </div>
+              </Tab>
+            </Tabs>
+          </form>
+        </div>
+
     );
   }
 }
