9de26d62238ca2881a81cd62f953e297d1bf822c
Philhawk <pkjacob@gmail.com>
2016-11-29 19:26:28 -0500

    activeLoops

--- app/components/StudentLandingPage/StudentLandingPage.jsx
+++ app/components/StudentLandingPage/StudentLandingPage.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import { Link } from 'react-router';
+import moment from 'moment';
 
 class StudentLandingPageComponent extends Component {
   constructor() {
@@ -7,20 +11,27 @@ class StudentLandingPageComponent extends Component {
   }
 
   render() {
+
     return (
-      <div className='container'>
-        <div className='row card'>
-          <h2>Join A Loop Below</h2>
-        </div>
-        {
-          this.props.activeSessions.map((session, i) => (
-            <div className='row card' key={i}>
-              <p className='card-title'>Lecture Title: {session.lecture.name}</p>
-              <p className='card-title'>Teacher: {session.lecture.teacher.name}</p>
-              <p className='card-title'>Link: <a href={session.bitly}>Join Loop</a></p>
-            </div>
-          ))
-        }
+        <div className='container'>
+          {console.log('WHAT IS THE ACTIVE SESSIONS', this.props.activeSessions)}
+          { this.props.activeSessions.map((session, i) => (
+            <Card className='card-wot'>
+              <CardMedia overlay={<CardTitle title={session.lecture.name} subtitle={session.lecture.teacher.name} />} >
+                <img src="https://source.unsplash.com/random/520x66" />
+              </CardMedia>
+              <CardTitle />
+              <CardText>
+                {session.lecture.description}
+              </CardText>
+              <CardActions>
+                <FlatButton label="Join Loop" href={session.bitly}> </FlatButton>
+                <FlatButton label={ `Started ${moment(session.timeStarted).from(moment(new Date()))}` }> </FlatButton>
+              </CardActions>
+
+            </Card>
+            ))
+          }
       </div>
     )
   }
@@ -30,3 +41,19 @@ const mapStateToProps = ({ activeSessions }) => ({ activeSessions })
 const StudentLandingPage = connect(mapStateToProps)(StudentLandingPageComponent)
 
 export default StudentLandingPage;
+
+
+//
+// <div className='row card' key={i}>
+// <p className='card-title'>Lecture Title: </p>
+// <p className='card-title'>Teacher: {session.lecture.teacher.name}</p>
+// <p className='card-title'>Link: <a href={session.bitly}>Join Loop</a></p>
+// </div>
+
+
+// <div >
+// <div className='row card'>
+// <h2>Join A Loop Below</h2>
+// </div>
+//
+// </div>
