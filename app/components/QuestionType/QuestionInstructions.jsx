5dffdd37f7efc13e7c932bf4f54be89be53bd787
Philhawk <pkjacob@gmail.com>
2016-11-30 08:44:07 -0500

    copy changes

--- app/components/QuestionType/QuestionInstructions.jsx
+++ app/components/QuestionType/QuestionInstructions.jsx
@@ -10,11 +10,11 @@ export default class QuestionInstructions extends Component {
   render() {
     return (
       <div>
-        <h5> Welcome! Let's create some questions.</h5>
+        <h4> Now, let's create some questions.</h4>
         <div className="instruction-text">
-          <p>Click <b>CREATE QUESTION</b> to make questions.</p>
+          <p>Click a <b>CREATE</b> button to make questions.</p>
           <p>Once you're satisfied with the question you've made, click <b>SAVE QUESTION</b> to add it to your queue.</p>
-          <p>When you are finished making questions, press the <b>START PRESENTATION</b> button to begin.</p>
+          <p>When you are finished making questions, press the <b>START PRESENTATION</b> button above to begin.</p>
         </div>
       </div>
     )
