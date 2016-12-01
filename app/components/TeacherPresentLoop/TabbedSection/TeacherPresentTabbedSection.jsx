c5e0757fa130807bef49c0c7ef98acf99c0728c8
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-12-01 12:30:34 -0500

    tabs are back

--- app/components/TeacherPresentLoop/TabbedSection/TeacherPresentTabbedSection.jsx
+++ app/components/TeacherPresentLoop/TabbedSection/TeacherPresentTabbedSection.jsx
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
 import TeacherPresentTabbedLeft from './TeacherPresentTabbedLeft';
 import TeacherPresentTabbedRight from './TeacherPresentTabbedRight';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
 export default class TeacherPresentTabbedSection extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'questions',
+    }
   }
 
+
+  handleChange = (value) => {
+    this.setState({
+      value: value,
+    });
+  };
+
   render() {
+
+   const styles = {
+      headline: {
+        fontSize: 24,
+        paddingTop: 16,
+        marginBottom: 12,
+        fontWeight: 400,
+      },
+    };
+
     return (
-        <div className="row card">
-            <div className="col s12 m8 l8 teacherPresentationTabbedSection card-title">
-              Questions
+      <Tabs value={this.state.value} onChange={this.handleChange}>
+        <Tab label="Questions" value="questions">
+          <div>
+            <h2 style={styles.headline}>Questions</h2>
               <TeacherPresentTabbedLeft />
-            </div>
-            <div className="col s12 m4 l4 teacherPresentationTabbedSection card-title">
-              Data
-             <TeacherPresentTabbedRight />
-            </div>
-        </div>
+          </div>
+        </Tab>
+        <Tab label="Data" value="data">
+          <div>
+            <h2 style={styles.headline}>Data</h2>
+              <TeacherPresentTabbedRight />
+          </div>
+        </Tab>
+        <Tab label="Mood" value="mood">
+          <div>
+            <h2 style={styles.headline}>Mood</h2>
+              <TeacherPresentTabbedRight />
+          </div>
+        </Tab>
+      </Tabs>
     );
   }
 }
