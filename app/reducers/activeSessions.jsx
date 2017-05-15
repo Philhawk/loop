cba29dec48780e0f0629c28be1711111ca04fc5e
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-14 22:12:27 -0400

    linted session reducer

--- app/reducers/activeSessions.jsx
+++ app/reducers/activeSessions.jsx
@@ -4,34 +4,34 @@ import axios from 'axios';
 const SET_ACTIVE_SESSIONS = 'SET_ACTIVE_SESSIONS';
 
 // sync action creators
-export const setActiveSessions = (sessionList) => ({ type: SET_ACTIVE_SESSIONS, sessionList })
+export const setActiveSessions = sessionList => ({ type: SET_ACTIVE_SESSIONS, sessionList });
 
 // Async action creators
-export const fetchActiveSessions = () => dispatch => {
+export const fetchActiveSessions = () => (dispatch) => {
   axios.get('/api/sessions/active')
-  .then(sessions => {
-    dispatch(setActiveSessions(sessions.data))
-  })
-}
+  .then((sessions) => {
+    dispatch(setActiveSessions(sessions.data));
+  });
+};
 
-export const fetchSessions = ({id}) => dispatch => {
+export const fetchSessions = ({ id }) => (dispatch) => {
   axios.get(`/api/sessions/lecture/${id}`)
-  .then(sessions => {
-    dispatch(setActiveSessions(sessions.data))
-  })
-}
+  .then((sessions) => {
+    dispatch(setActiveSessions(sessions.data));
+  });
+};
 
 // initial state
 const initialState = [];
 
 // reducer
-const reducer = (state=initialState, action) => {
-  switch(action.type) {
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
     case SET_ACTIVE_SESSIONS:
-      return action.sessionList
+      return action.sessionList;
     default:
       return state;
   }
-}
+};
 
 export default reducer;
