af95f71b7252a68825d706d66de11ba1b5fe4983
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-15 19:51:17 -0400

    linted

--- app/reducers/currentQuestion.jsx
+++ app/reducers/currentQuestion.jsx
@@ -1,29 +1,28 @@
 
 // Constants
-const SET_CURRENT_QUESTION = "SET_CURRENT_QUESTION";
+const SET_CURRENT_QUESTION = 'SET_CURRENT_QUESTION';
 
 // Sync Action Creators
-const setCurrentQuestion = question => ({ type: SET_CURRENT_QUESTION, question })
+const setCurrentQuestion = question => ({ type: SET_CURRENT_QUESTION, question });
 
 // Async Action Creators
-
-export const callSetCurrentQuestion = (question) => dispatch => {
-  dispatch(setCurrentQuestion(question))
-}
+export const callSetCurrentQuestion = question => (dispatch) => {
+  dispatch(setCurrentQuestion(question));
+};
 
 // Initial State
 const initialState = {
   content: 'Questions Will Appear Here',
   choices: [],
-}
+};
 
-const reducer = (state=initialState, action) => {
-  switch(action.type) {
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
     case SET_CURRENT_QUESTION:
       return action.question;
     default:
       return state;
   }
-}
+};
 
 export default reducer;
