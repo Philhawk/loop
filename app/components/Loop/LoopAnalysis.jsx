743d8345805c41245220aa3fd7c6a59527a455bf
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-14 21:56:15 -0400

    removed unused imports

--- app/components/Loop/LoopAnalysis.jsx
+++ app/components/Loop/LoopAnalysis.jsx
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
-import {Tabs, Tab} from 'material-ui/Tabs';
-import Slider from 'material-ui/Slider';
-import { Button }  from 'react-materialize';
+import { Tabs, Tab } from 'material-ui/Tabs';
+import { Button } from 'react-materialize';
 import { browserHistory } from 'react-router';
-import {List, ListItem} from 'material-ui/List';
+import { List, ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
-import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
-import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
-import IconButton from 'material-ui/IconButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import {Card, CardActions, CardMedia, CardHeader, CardText} from 'material-ui/Card';
+import { darkBlack } from 'material-ui/styles/colors';
+import { Card, CardMedia, CardText } from 'material-ui/Card';
 import { updateSession } from '../../reducers/session';
 import { callClearStudentQuestions } from '../../reducers/studentQuestions';
 
@@ -23,28 +17,28 @@ class LoopAnalysis extends Component {
     super();
     this.state = {
       sessionLength: '',
-    }
+    };
     this.onReturnHome = this.onReturnHome.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
     let timeNow = moment();
-    let lectureStart = moment(this.props.session.timeStarted)
-    let duration = moment.duration(timeNow - lectureStart)
-    let formattedTimeDuration = moment(duration.asMilliseconds()).format('mm:ss')
-    this.setState({ sessionLength: formattedTimeDuration })
+    let lectureStart = moment(this.props.session.timeStarted);
+    let duration = moment.duration(timeNow - lectureStart);
+    let formattedTimeDuration = moment(duration.asMilliseconds()).format('mm:ss');
+    this.setState({ sessionLength: formattedTimeDuration });
   }
 
   onReturnHome(e) {
     e.preventDefault();
-    this.props.callClearStudentQuestions()
+    this.props.callClearStudentQuestions();
     this.props.updateSession({
       session_id: this.props.session.id,
-      sessionLength: this.state.sessionLength
+      sessionLength: this.state.sessionLength,
     })
     .then(() => {
       browserHistory.push("/profile/previousLoops");
-    })
+    });
   }
 
   render() {
@@ -65,37 +59,37 @@ class LoopAnalysis extends Component {
             <Tabs>
               <Tab label="Statistics" >
               <Card>
-                <CardText style={{color: 'white', backgroundColor: 'teal', margin: '1em 0 1em 0', paddingBottom: '1em'}}>
+                <CardText style={{ color: 'white', backgroundColor: 'teal', margin: '1em 0 1em 0', paddingBottom: '1em' }}>
                   Duration:
                   <h1>{this.state.sessionLength}</h1>
                 </CardText>
                 <CardMedia></CardMedia>
               </Card>
 
-              <Card style={{border: '100em'}}>
-                <CardText style={{color: 'white', backgroundColor: 'maroon', margin: '1em 0 1em 0', paddingBottom: '1em'}}>
+              <Card style={{ border: '100em' }}>
+                <CardText style={{ color: 'white', backgroundColor: 'maroon', margin: '1em 0 1em 0', paddingBottom: '1em' }}>
                   Total Questions Asked:
                   <h1>{this.props.studentQuestions.length}</h1>
                 </CardText>
                 <CardMedia></CardMedia>
               </Card>
 
-              <Card style={{border: '100em'}}>
-                <CardText style={{color: 'white', backgroundColor: 'navy', margin: '1em 0 1em 0', paddingBottom: '1em'}}>
+              <Card style={{ border: '100em' }}>
+                <CardText style={{ color: 'white', backgroundColor: 'navy', margin: '1em 0 1em 0', paddingBottom: '1em' }}>
                   Answered Ratio:
                   <h1>{Math.round(this.props.answeredQuestions/this.props.studentQuestions.length * 100)}%</h1>
                 </CardText>
                 <CardMedia></CardMedia>
               </Card>
 
-              <Card style={{border: '100em'}}>
-                <CardText style={{color: 'white', backgroundColor: 'blue', margin: '1em 0 1em 0', paddingBottom: '1em'}}>
+              <Card style={{ border: '100em' }}>
+                <CardText style={{ color: 'white', backgroundColor: 'blue', margin: '1em 0 1em 0', paddingBottom: '1em' }}>
                   Final Student Mood:
                   <h1>{this.props.studentMood}</h1>
                 </CardText>
                 <CardMedia></CardMedia>
               </Card>
-                <Button waves='light' className="#546e7a blue-grey darken-1 return-home-finish" onClick={this.onReturnHome}>
+                <Button waves="light" className="#546e7a blue-grey darken-1 return-home-finish" onClick={this.onReturnHome}>
                   Return Home
                 </Button>
               </Tab>
@@ -110,7 +104,7 @@ class LoopAnalysis extends Component {
                           secondaryText={
                             <p>
                               <br/>
-                              <span style={{color: darkBlack}}>{question.content}</span>
+                              <span style={{ color: darkBlack }}>{question.content}</span>
 
                             </p>
                           }
@@ -130,8 +124,8 @@ class LoopAnalysis extends Component {
   }
 }
 
-const mapStateToProps = ({ session, studentQuestions, studentMood, answeredQuestions}) => ({ session, studentQuestions, studentMood, answeredQuestions })
-const mapDispatchToProps = { updateSession, callClearStudentQuestions }
-const Loop = connect(mapStateToProps, mapDispatchToProps)(LoopAnalysis)
+const mapStateToProps = ({ session, studentQuestions, studentMood, answeredQuestions }) => ({ session, studentQuestions, studentMood, answeredQuestions });
+const mapDispatchToProps = { updateSession, callClearStudentQuestions };
+const Loop = connect(mapStateToProps, mapDispatchToProps)(LoopAnalysis);
 
 export default Loop;
