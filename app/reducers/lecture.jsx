a1be4bd7c06d5882530bba881d99c0b6b7e06319
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-15 19:58:52 -0400

    semis and brackets

--- app/reducers/lecture.jsx
+++ app/reducers/lecture.jsx
@@ -6,44 +6,44 @@ const SET_LECTURE = 'SET_LECTURE';
 
 export const setLecture = lecture => ({
   type: SET_LECTURE,
-  lecture
-})
+  lecture,
+});
 
-export const fetchLecture = ({lecture_id}) => dispatch => {
+export const fetchLecture = ({ lecture_id }) => (dispatch) => {
   return axios.get(`/api/lectures/${lecture_id}`)
-  .then(lecture => {
-    dispatch(setLecture(lecture.data))
-  })
-}
+  .then((lecture) => {
+    dispatch(setLecture(lecture.data));
+  });
+};
 
-export const createLecture = ({ name, mood, description, teacher_id }) => dispatch => {
+export const createLecture = ({ name, mood, description, teacher_id }) => (dispatch) => {
   return axios.post('/api/lectures', { name, mood, description, teacher_id })
-  .then(lecture => {
-    dispatch(setLecture(lecture.data))
-  })
-}
+  .then((lecture) => {
+    dispatch(setLecture(lecture.data));
+  });
+};
 
-export const updateLecture = ({ name, mood, description, teacher_id, lecture_id }) => dispatch => {
+export const updateLecture = ({ name, mood, description, teacher_id, lecture_id }) => (dispatch) => {
   axios.put(`/api/lectures/${lecture_id}`, { name, mood, description, teacher_id })
   .then(lecture => {
-    dispatch(setLecture(lecture.data))
-  })
-}
+    dispatch(setLecture(lecture.data));
+  });
+};
 
-export const deleteLecture = ({ lecture_id }) => dispatch => {
+export const deleteLecture = ({ lecture_id }) => (dispatch) => {
   return axios.delete(`/api/lectures/${lecture_id}`)
   .then(() => {
-  })
-}
+  });
+};
 
-const initialState = { id: 0, name: "", mood: 0, timeStarted: null}
+const initialState = { id: 0, name: '', mood: 0, timeStarted: null };
 
 const reducer = (state = initialState, { lecture, type }) => {
-  switch(type) {
+  switch (type) {
     case SET_LECTURE:
       return lecture;
   }
   return state;
-}
+};
 
 export default reducer;
