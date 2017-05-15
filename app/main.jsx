f7b6d19aca52f960e727aa8b1eae3e2f1fba9fe4
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-14 21:10:54 -0400

    cleaned up main.jsx

--- app/main.jsx
+++ app/main.jsx
@@ -1,12 +1,10 @@
-'use strict'
-import React from 'react'
-import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
-import {render} from 'react-dom'
-import {connect, Provider} from 'react-redux'
-import Routes from './routes'
-import store from './store'
+import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from "react-tap-event-plugin";
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import Routes from './routes';
+import store from './store';
 
 injectTapEventPlugin();
 
@@ -16,5 +14,5 @@ render (
       <Routes/>
     </MuiThemeProvider>
   </Provider>,
-  document.getElementById('main')
-)
+  document.getElementById('main'),
+);
