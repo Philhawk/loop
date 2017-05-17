ad59585321592c85a52839bfb752c17744fc8767
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-16 21:29:20 -0400

    uniform spaces

--- app/reducers/openEndedAnswers.jsx
+++ app/reducers/openEndedAnswers.jsx
@@ -1,32 +1,28 @@
-import axios from 'axios';
-
-
 // actions
-const ADD_ANSWER = "ADD_ANSWER";
-const RESET = "RESET";
+const ADD_ANSWER = 'ADD_ANSWER';
+const RESET = 'RESET';
 
-export const addAnswer = answer => ({ type: ADD_ANSWER, answer })
-export const reset = () => ({type: RESET })
+export const addAnswer = answer => ({ type: ADD_ANSWER, answer });
+export const reset = () => ({ type: RESET });
 
-export const callAddAnswer = answer => dispatch => {
-  dispatch(addAnswer(answer))
-}
+export const callAddAnswer = answer => (dispatch) => {
+  dispatch(addAnswer(answer));
+};
 
-export const callOpenEndedReset = () => dispatch => {
-  dispatch(reset())
-}
+export const callOpenEndedReset = () => (dispatch) => {
+  dispatch(reset());
+};
 
 const initialState = [];
 
 const reducer = (state = initialState, action) => {
-
-  switch(action.type) {
+  switch (action.type) {
     case ADD_ANSWER:
-      return state.concat(action.answer)
+      return state.concat(action.answer);
     case RESET:
       return initialState;
   }
   return state;
-}
+};
 
 export default reducer;
