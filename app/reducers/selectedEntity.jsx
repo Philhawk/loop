1f76582cc3d748beb3293fb2e4bf90de038608eb
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-17 22:10:15 -0400

    linted selectedEntity

--- app/reducers/selectedEntity.jsx
+++ app/reducers/selectedEntity.jsx
@@ -1,29 +1,29 @@
 import axios from 'axios';
 
 // constants
-const SET_ENTITY = "SET_ENTITY";
+const SET_ENTITY = 'SET_ENTITY';
 
 // sync action creators
-export const setEntity = entity => ({ type: SET_ENTITY, entity })
+export const setEntity = entity => ({ type: SET_ENTITY, entity });
 
 // async action creators
-export const fetchEntity = (entityId) => dispatch => {
-  console.log("ID IN SELECTED ENTITY REDUCER", entityId)
+export const fetchEntity = entityId => (dispatch) => {
+  console.log('ID IN SELECTED ENTITY REDUCER', entityId);
   axios.get(`/api/entities/${entityId}`)
-  .then(entity => {
-    dispatch(setEntity(entity.data))
-  })
-}
+  .then((entity) => {
+    dispatch(setEntity(entity.data));
+  });
+};
 
 const initialState = {};
 
 const reducer = (state = initialState, { entity, type }) => {
-  switch(type) {
+  switch (type) {
     case SET_ENTITY:
       return entity;
     default:
-      return state
+      return state;
   }
-}
+};
 
 export default reducer;
