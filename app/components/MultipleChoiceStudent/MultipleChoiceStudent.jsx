e46a15081eb6716311b24a4dd802168f550c8dc7
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-18 14:43:28 -0400

    cleaned Multiple choice student

--- app/components/MultipleChoiceStudent/MultipleChoiceStudent.jsx
+++ app/components/MultipleChoiceStudent/MultipleChoiceStudent.jsx
@@ -4,33 +4,33 @@ import { Button } from 'react-materialize';
 
 class MultipleChoiceStudentComponent extends Component {
   constructor(props) {
-    super(props)
-    this.state = { selected: -1, submitted: false, correctAnswer: -1}
+    super(props);
+    this.state = { selected: -1, submitted: false, correctAnswer: -1 };
     this.onSubmitAnswer = this.onSubmitAnswer.bind(this);
 
     this.props.socket.on('newTeacherQuestion', ({ question }) => {
       if (question.questionType === 'multipleChoice') {
-        this.setState({submitted: false})
-        this.setState({ selected: -1, correctAnswer: -1 })
+        this.setState({ submitted: false });
+        this.setState({ selected: -1, correctAnswer: -1 });
       }
-    })
-    this.props.socket.on('studentReceieveAnswer' , ({ correctAnswer, questionType }) => {
-      if(questionType === 'multipleChoice') {
-        this.setState({ correctAnswer })
+    });
+    this.props.socket.on('studentReceieveAnswer', ({ correctAnswer, questionType }) => {
+      if (questionType === 'multipleChoice') {
+        this.setState({ correctAnswer });
       }
-    })
+    });
   }
 
   onSubmitAnswer() {
-    this.props.socket.emit('submitMultipleChoice', {answer: this.state.selected, sessionString: this.props.session.sessionString})
-    this.setState({submitted: true})
+    this.props.socket.emit('submitMultipleChoice', { answer: this.state.selected, sessionString: this.props.session.sessionString });
+    this.setState({ submitted: true });
   }
 
   showButton() {
     if (this.state.submitted) {
-      return <Button className="disabled submit-button-student">Submitted</Button>
+      return <Button className="disabled submit-button-student">Submitted</Button>;
     } else {
-     return <Button className="submit-button-student #0d47a1 blue darken-4" onClick={this.onSubmitAnswer}>Submit Answer</Button>
+     return <Button className="submit-button-student #0d47a1 blue darken-4" onClick={this.onSubmitAnswer}>Submit Answer</Button>;
     }
   }
 
@@ -61,9 +61,8 @@ class MultipleChoiceStudentComponent extends Component {
   }
 }
 
-const mapStateToProps = ({ currentQuestion, socket, session }) => ({ currentQuestion, socket, session})
+const mapStateToProps = ({ currentQuestion, socket, session }) => ({ currentQuestion, socket, session});
 
-const MultipleChoiceStudent = connect(mapStateToProps)(MultipleChoiceStudentComponent)
+const MultipleChoiceStudent = connect(mapStateToProps)(MultipleChoiceStudentComponent);
 
 export default MultipleChoiceStudent;
-                  // { Number(this.state.correctAnswer) === index ? <p style={{color: 'green'}}>Correct Answer!</p> : null }
