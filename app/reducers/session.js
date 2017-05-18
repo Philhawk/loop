405905c2c539f6edc3dde38043e4277f1fa35d80
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-18 11:20:33 -0400

    corrected bracket spacing and semis

--- app/reducers/session.js
+++ app/reducers/session.js
@@ -1,68 +1,68 @@
 import axios from 'axios';
 
 // Constants
-const SET_CURRENT_SESSION = "SET_CURRENT_SESSION";
+const SET_CURRENT_SESSION = 'SET_CURRENT_SESSION';
 
 // Sync Action Creators
-export const setCurrentSession = session => ({ type: SET_CURRENT_SESSION, session })
-
+export const setCurrentSession = session => ({ type: SET_CURRENT_SESSION, session });
 
 // Async Action Creators
-export const createSession = ({ sessionString, active, lecture_id, bitly }) => dispatch => {
+export const createSession = ({ sessionString, active, lecture_id, bitly }) => (dispatch) => {
   axios.post('/api/sessions', { sessionString, active, lecture_id, bitly })
-  .then(session => {
-    dispatch(setCurrentSession(session.data))
+  .then((session) => {
+    dispatch(setCurrentSession(session.data));
   })
   .catch(err => console.error(err));
-}
+};
 
-export const fetchCurrentSession = ({ sessionString }) => dispatch => {
+export const fetchCurrentSession = ({ sessionString }) => (dispatch) => {
   return axios.get(`/api/sessions/string/${sessionString}`)
-  .then(session => {
-    dispatch(setCurrentSession(session.data))
+  .then((session) => {
+    dispatch(setCurrentSession(session.data));
   })
   .catch(err => console.error(err));
-}
+};
 
-export const updateSession = ({ session_id, sessionLength }) => dispatch => {
+export const updateSession = ({ session_id, sessionLength }) => (dispatch) => {
   return axios.put(`/api/sessions/${session_id}`, { sessionLength, active: false })
-  .then(session => {
-  })
-}
+  .then((session) => {
+  });
+};
 
-export const activateSession = ({session_id}) => dispatch => {
+export const activateSession = ({ session_id }) => (dispatch) => {
   axios.put(`/api/sessions/${session_id}/activate`, { timeStarted: Date.now(), active: true })
-  .then(session => {
-    dispatch(setCurrentSession(session.data))
+  .then((session) => {
+    dispatch(setCurrentSession(session.data));
   })
   .catch(err => console.error(err));
-}
+};
 
-export const createActiveSession = ({ sessionString, lecture_id }) => dispatch => {
+export const createActiveSession = ({ sessionString, lecture_id }) => (dispatch) => {
   return axios.post('/api/sessions/active', {
     sessionString,
     lecture_id,
     active: true,
-    timeStarted: Date.now()
+    timeStarted: Date.now(),
   })
-  .then(session => {
-    dispatch(setCurrentSession(session.data))
-  })
-}
+  .then((session) => {
+    dispatch(setCurrentSession(session.data));
+  });
+};
 
-export const endSession = ({ session_id, sessionLength, active }) => dispatch => {
+export const endSession = ({ session_id, sessionLength, active }) => (dispatch) => {
   axios.put(`/api/sessions/${session_id}`, { active, sessionLength })
   .then(session => {
-    dispatch(setCurrentSession(session.data))
+    dispatch(setCurrentSession(session.data));
   })
-  .catch(err => console.error(err));}
+  .catch(err => console.error(err));
+};
 
-  export const deleteSession = ({ session_id }) => dispatch => {
+  export const deleteSession = ({ session_id }) => (dispatch) => {
     axios.delete(`/api/sessions/${session_id}`)
     .then(() => {
       console.log('SESSION DESTROYED');
-    })
-  }
+    });
+  };
 
 // Initial State
 const initialState = {
@@ -70,16 +70,16 @@ const initialState = {
   active: false,
   lecture_id: null,
   bitly: '',
-  currentQuestion: -1
-}
+  currentQuestion: -1,
+};
 
-const reducer = (state=initialState, action) => {
-  switch(action.type) {
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
     case SET_CURRENT_SESSION:
       return action.session;
     default:
       return state;
   }
-}
+};
 
 export default reducer;
