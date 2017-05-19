a3629733a1e11669c9b2678119ce4210f58e43f5
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-19 18:24:04 -0400

    reverted questions and sessions in server

--- server/questions.js
+++ server/questions.js
@@ -8,25 +8,31 @@ const questionsRouter = express.Router();
 // get all questions
 questionsRouter.get('/', (req, res, next) => {
   db.model('questions').findAll()
-    .then(questions => res.json(questions))
-    .catch(next);
+  .then(questions => {
+    res.json(questions)
+  })
+  .catch(next);
 });
 
 // get a specific question by its ID
 questionsRouter.get('/:questionId', (req, res, next) => {
   db.model('questions').findById(req.params.questionId)
-    .then(question => res.json(question))
-    .catch(next);
+  .then(question => {
+    res.json(question);
+  })
+  .catch(next);
 });
 
 // get all questions associated with a specific lecture
 questionsRouter.get('/lecture/:lectureId', (req, res, next) => {
   db.model('questions').findAll({
     where: {
-      lecture_id: req.params.lectureId,
-    },
+      lecture_id: req.params.lectureId
+    }
   })
-  .then(questions => res.json(questions))
+  .then(questions => {
+    res.json(questions)
+  })
   .catch(next);
 });
 
@@ -35,17 +41,17 @@ questionsRouter.get('/session/:sessionString', (req, res, next) => {
   db.model('sessions').findOne({
     include: [{ model: db.model('lectures') }],
     where: {
-      sessionString: req.params.sessionString,
-    },
+      sessionString: req.params.sessionString
+    }
   })
-  .then((session) => {
+  .then(session => {
     db.model('questions').findAll({
       where: {
-        lecture_id: session.lecture.id,
-      },
+        lecture_id: session.lecture.id
+      }
     })
-    .then((questions) => {
-      res.json(questions);
+    .then(questions => {
+      res.json(questions)
     })
   })
   .catch(next);
@@ -54,26 +60,32 @@ questionsRouter.get('/session/:sessionString', (req, res, next) => {
 // create a question
 questionsRouter.post('/', (req, res, next) => {
   db.model('questions').create(req.body)
-    .then(question => res.status(201).json(question))
-    .catch(next);
+  .then(question => {
+    res.status(201).json(question)
+  })
+  .catch(next);
 });
 
 // update a specific question
 questionsRouter.put('/:questionId', (req, res, next) => {
   db.model('questions').findById(req.params.questionId)
-    .then(question => question.update(req.body))
-    .then(updatedQuestion => res.status(201).send(updatedQuestion))
-    .catch(next);
-});
+  .then(question => {
+    question.update(req.body)
+    .then(updatedQuestion => {
+      res.status(201).send(updatedQuestion)
+    })
+  })
+  .catch(next);
+})
 
 // deletes a specific question
 questionsRouter.delete('/:questionId', (req, res, next) => {
   db.model('questions').findById(req.params.questionId)
-    .then((question) => {
-      question.destroy()
-      .then(() => res.sendStatus(204))
-    })
-    .catch(next);
-});
+  .then(question => {
+    question.destroy()
+    .then(() => res.sendStatus(204))
+  })
+  .catch(next);
+})
 
 module.exports = questionsRouter;
