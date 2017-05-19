14bf082eebae1348edf659069fe232ae7cbd4824
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-19 18:27:43 -0400

    reverted start.js;

--- server/start.js
+++ server/start.js
@@ -1,22 +1,22 @@
 'use strict'
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const { resolve } = require('path');
-const passport = require('passport');
+const express = require('express')
+const bodyParser = require('body-parser')
+const {resolve} = require('path')
+const passport = require('passport')
 
 // Bones has a symlink from node_modules/APP to the root of the app.
 // That means that we can require paths relative to the app root by
 // saying require('APP/whatever').
 //
 // This next line requires our root index.js:
-const pkg = require('APP');
+const pkg = require('APP')
 
-const app = express();
+const app = express()
 
 if (!pkg.isProduction && !pkg.isTesting) {
   // Logging middleware (dev only)
-  app.use(require('volleyball'));
+  app.use(require('volleyball'))
 }
 
 module.exports = app
@@ -65,48 +65,48 @@ if (module === require.main) {
 
 
     socket.on('loopCreated', ({ loopUuId, role }) => {
-      socket.join(loopUuId);
-      console.log(`A ${role} just joined loop ${loopUuId}`);
+      socket.join(loopUuId)
+      console.log(`A ${role} just joined loop ${loopUuId}`)
       if (role === 'Student') {
-        io.in(loopUuId).emit('studentJoined');
+        io.in(loopUuId).emit('studentJoined')
       }
     })
 
     socket.on('studentAsk', ({ question, sessionString }) => {
       console.log(question)
-      io.in(sessionString).emit('newStudentQuestion', { question });
-    });
+      io.in(sessionString).emit('newStudentQuestion', { question })
+    })
 
     socket.on('studentMoodClick', ({ mood, sessionString }) => {
-      io.in(sessionString).emit('studentMoodIndex', { mood });
-    });
+      io.in(sessionString).emit('studentMoodIndex', { mood })
+    })
 
     socket.on('teacherAsk', ({ question, sessionString }) => {
-      io.in(sessionString).emit('newTeacherQuestion', { question });
-    });
+      io.in(sessionString).emit('newTeacherQuestion', { question })
+    })
 
     socket.on('submitMultipleChoice', ({ answer, sessionString }) => {
-      io.in(sessionString).emit('studentMultipleChoiceAnswer', { answer });
-    });
+      io.in(sessionString).emit('studentMultipleChoiceAnswer', { answer })
+    })
 
     socket.on('submitOpenEnded', ({ answer, sessionString }) => {
-      io.in(sessionString).emit('studentOpenEndedAnswer', { answer });
-    });
+      io.in(sessionString).emit('studentOpenEndedAnswer', { answer })
+    })
 
     socket.on('teacherSendAnswer', ({ correctAnswer, sessionString, questionType }) => {
       console.log("CORRECTANSWER", correctAnswer)
-      io.in(sessionString).emit('studentReceieveAnswer', { correctAnswer, questionType });
-    });
+      io.in(sessionString).emit('studentReceieveAnswer' , { correctAnswer, questionType })
+    })
 
     socket.on('teacherEndsLecture', ({ sessionString }) => {
-      io.in(sessionString).emit('endStudentLecture');
-    });
+      io.in(sessionString).emit('endStudentLecture')
+    })
 
     socket.on('disconnecting', () => {
-      const rooms = socket.rooms;
-      for (let room in rooms) {
-        io.in(room).emit('studentLeft');
+      let rooms = socket.rooms
+      for(let room in rooms) {
+        io.in(room).emit('studentLeft')
       }
-    });
-  });
+    })
+  })
 }
