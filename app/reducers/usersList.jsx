db53629ebbad7af1abb7bee7b3522d7fb5612e95
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-19 13:13:10 -0400

    fixed the number of arguments

--- app/reducers/usersList.jsx
+++ app/reducers/usersList.jsx
@@ -7,33 +7,33 @@ const DELETE_USER = 'DELETE_USER';
 // sync action creator
 export const setUsers = users => ({
   type: SET_USERS,
-  users
+  users,
 });
 
 // async action creator
-export const fetchUsersByEntity = (entityId) => dispatch => {
-  console.log("ENTITY ID IN USERSLIST REDUCER", entityId)
+export const fetchUsersByEntity = (entityId) => (dispatch) => {
+  console.log('ENTITY ID IN USERSLIST REDUCER', entityId);
   axios.get(`/api/users/entity/${entityId}`)
-  .then(users => {
-    dispatch(setUsers(users.data))
-  })
+  .then((users) => {
+    dispatch(setUsers(users.data));
+  });
 };
 
-export const removeUserFromDatabase = (userId) => dispatch => {
-  return axios.delete(`/api/users/${userId}`)
-}
+export const removeUserFromDatabase = (userId) => (dispatch) => {
+  return axios.delete(`/api/users/${userId}`);
+};
 
 // initial state
 const initialState = [];
 
 // reducer
-const reducer = (state = initialState, {users, type}) => {
-  switch(type) {
+const reducer = (state = initialState, { users, type }) => {
+  switch (type) {
     case SET_USERS:
       return users;
     default:
       return state;
   }
-}
+};
 
 export default reducer;
