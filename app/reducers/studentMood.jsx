fa75d826f0c539a24c1de144718301c22b902252
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-22 18:45:22 -0400

    fixed destructure spaces

--- app/reducers/studentMood.jsx
+++ app/reducers/studentMood.jsx
@@ -1,32 +1,32 @@
 import axios from 'axios';
 
 // Constants
-const STUDENT_ADD_MOOD = "STUDENT_ADD_MOOD";
+const STUDENT_ADD_MOOD = 'STUDENT_ADD_MOOD';
 
 // Sync Action Creators
-export const studentAddMood = ({mood}) => ({ type: STUDENT_ADD_MOOD, mood })
+export const studentAddMood = ({ mood }) => ({ type: STUDENT_ADD_MOOD, mood });
 
 // Async Action Creators
-export const callStudentAddMood = mood => dispatch => {
-  dispatch(studentAddMood(mood))
-}
+export const callStudentAddMood = mood => (dispatch) => {
+  dispatch(studentAddMood(mood));
+};
 
-export const putMood = ({ sessionId, mood }) => dispatch => {
+export const putMood = ({ sessionId, mood }) => (dispatch) => {
   axios.put(`/api/sessions/${sessionId}/mood`, { mood })
   .then((session) => {
-  })
-}
+  });
+};
 
 // Initial State
 const initialState = 0;
 
-const reducer = (state=initialState, action) => {
-  switch(action.type) {
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
     case STUDENT_ADD_MOOD:
-      return state + action.mood
+      return state + action.mood;
     default:
       return state;
   }
-}
+};
 
 export default reducer;
