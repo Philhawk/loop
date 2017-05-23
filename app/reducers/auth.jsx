1d247dc7dfaaa9342ba756aa4c3a6f0b119ec4bd
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-22 22:11:24 -0400

    moved action types above the reducer

--- app/reducers/auth.jsx
+++ app/reducers/auth.jsx
@@ -1,49 +1,50 @@
-import axios from 'axios'
+import axios from 'axios';
+
+// Action types
+const CREATE_USER = 'CREATE_USER';
+const AUTHENTICATED = 'AUTHENTICATED';
 
 const initialState = {
   id: 0,
   name: '',
   email: '',
-  role: ''
-}
+  role: '',
+};
 
-const reducer = (state=initialState, action) => {
-  switch(action.type) {
-  case CREATE_USER:
-    return action.user
-  case AUTHENTICATED:
-    return action.user
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_USER:
+      return action.user;
+    case AUTHENTICATED:
+      return action.user;
+    default: return state;
   }
-  return state
-}
-
-const CREATE_USER = 'CREATE_USER'
-const AUTHENTICATED = 'AUTHENTICATED'
+};
 
 export const authenticated = user => ({
-  type: AUTHENTICATED, user
-})
+  type: AUTHENTICATED, user,
+});
 
 export const createUser = user => ({
-  type: CREATE_USER, user
-})
+  type: CREATE_USER, user,
+});
 
 export const signup = ({ email, password, name, role }) => dispatch => {
   axios.post('/api/users', { email, password, name, role })
-  .then(user => dispatch(createUser(user.data)))
-}
+  .then(user => dispatch(createUser(user.data)));
+};
 
 export const login = (email, password) => {
   return (dispatch) => axios.post('/api/auth/local/login', {email, password})
       .then(() => dispatch(whoami()))
-      .catch(err => {throw new Error(err)})
-}
+      .catch(err => {throw new Error(err)});
+};
 
 export const logout = () =>
   dispatch =>
     axios.post('/api/auth/logout')
       .then(() => dispatch(whoami()))
-      .catch(() => dispatch(whoami()))
+      .catch(() => dispatch(whoami()));
 
 export const whoami = () =>
   dispatch =>
@@ -52,6 +53,6 @@ export const whoami = () =>
         const user = response.data
         dispatch(authenticated(user))
       })
-      .catch(failed => dispatch(authenticated(null)))
+      .catch(failed => dispatch(authenticated(null)));
 
-export default reducer
+export default reducer;
