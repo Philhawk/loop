b408643effa0a98028a96396c35f205c74fb8933
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-25 22:21:33 -0400

    aligned terms and conditions right

--- app/components/HomePage/Footer.jsx
+++ app/components/HomePage/Footer.jsx
@@ -8,12 +8,12 @@ class Footer extends Component {
     return (
       <div className="footer">
         <div className="row">
-          <div className="col s4 m4 l4">
+          <div className="col s6 m6 l6">
             <div className="bottom-brand-logo">
               {<img src="/logo.png" alt="Logo of Loop"></img>}   Loop
             </div>
           </div>
-          <div className="col s8 m8 l8 termsandconditions">
+          <div className="col s6 m6 l6 termsandconditions">
             Terms & Privacy
           </div>
 
