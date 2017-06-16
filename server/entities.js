c3cf69fd95c6659ce0b3d9977759c5b76eba6bd6
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-06-16 17:04:02 -0400

    linted entity.js, unified the brackets

--- server/entities.js
+++ server/entities.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const express = require('express');
 const db = require('APP/db');
 
@@ -8,26 +6,26 @@ const entitiesRouter = express.Router();
 // get all entities
 entitiesRouter.get('/', (req, res, next) => {
   db.model('entities').findAll()
-  .then(entities => {
+  .then((entities) => {
     res.json(entities);
   })
   .catch(next);
-})
+});
 
 // get a specific entity
 entitiesRouter.get('/:entityId', (req, res, next) => {
   db.model('entities').findById(req.params.entityId)
-  .then(entity => {
+  .then((entity) => {
     res.json(entity);
   })
   .catch(next);
-})
+});
 
 // create an entity
 entitiesRouter.post('/', (req, res, next) => {
   db.model('entities').create(req.body)
-  .then(entity => {
-    res.status(201).json(entity)
+  .then((entity) => {
+    res.status(201).json(entity);
   })
   .catch(next);
 });
@@ -35,23 +33,23 @@ entitiesRouter.post('/', (req, res, next) => {
 // update a specific entity
 entitiesRouter.put('/:entityId', (req, res, next) => {
   db.model('entities').findById(req.params.entityId)
-  .then(entity => {
+  .then((entity) => {
     entity.update(req.body)
-    .then(updatedEntity => {
-      res.status(201).send(updatedEntity)
-    })
+    .then((updatedEntity) => {
+      res.status(201).send(updatedEntity);
+    });
   })
   .catch(next);
-})
+});
 
 // delete a specific entity
 entitiesRouter.delete('/:entityId', (req, res, next) => {
   db.model('entities').findById(req.params.entityId)
-  .then(entity => {
+  .then((entity) => {
     entity.destroy()
-    .then(() => res.sendStatus(204))
+    .then(() => res.sendStatus(204));
   })
   .catch(next);
-})
+});
 
 module.exports = entitiesRouter;
