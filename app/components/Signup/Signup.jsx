071d0b8a5a1102d50e7fc02e821fc67888b1369f
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-06-24 10:14:45 -0400

    self closing for empty <hr>

--- app/components/Signup/Signup.jsx
+++ app/components/Signup/Signup.jsx
@@ -12,13 +12,14 @@ class SignUpComponent extends Component {
   }
 
   onSignup(e) {
-    e.preventDefault()
+    e.preventDefault();
     this.props.signup({
       email: e.target.email.value,
       password: e.target.password.value,
       name: e.target.name.value,
-      role: e.target.role.value})
-    browserHistory.push('/loop')
+      role: e.target.role.value,
+    });
+    browserHistory.push('/loop');
   }
 
   render() {
@@ -26,23 +27,23 @@ class SignUpComponent extends Component {
       <div>
         <form className="form-signin" onSubmit={this.onSignup}>
           <h3 className="form-signin-heading">Sign Up</h3>
-          <hr className="colorgraph"></hr>
+          <hr className="colorgraph" />
           <Input className="form-control" name="name" type="" placeholder="Name" s={12} />
           <Input className="form-control" name="email" type="email" placeholder="Email" s={12} />
           <Input className="form-control" name="password" type="password" placeholder="Password" s={12} />
           <Input className="form-control" name="role" type="" placeholder="Role" s={12} id="role-field" />
-          <Button waves='light' className="#0d47a1 blue darken-4 login-and-submit-button">Submit</Button>
-          <Button waves='light' className="#546e7a blue-grey darken-1 return-home-signin">
+          <Button waves="light" className="#0d47a1 blue darken-4 login-and-submit-button">Submit</Button>
+          <Button waves="light" className="#546e7a blue-grey darken-1 return-home-signin">
             <Link to="/" className="white-text">Return Home</Link>
           </Button>
 
         </form>
       </div>
-    )
+    );
   }
 }
 
-const mapDispatchToProps = { signup }
-const Signup = connect(null, mapDispatchToProps)(SignUpComponent)
+const mapDispatchToProps = { signup };
+const Signup = connect(null, mapDispatchToProps)(SignUpComponent);
 
 export default Signup;
